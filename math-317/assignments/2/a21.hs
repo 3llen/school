e66f54df00427384ebbdca2ad2eb0dd691015989
Jacob Errington <git@mail.jerrington.me>
2017-10-23 17:34:09 -0400

    math 317 assignment 2: complete

--- math-317/assignments/2/a21.hs
+++ math-317/assignments/2/a21.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ViewPatterns #-}
 
-import Data.List (intercalate)
+import Data.Ord (comparing)
+import Data.List (intercalate, sortBy, zip6)
 import GHC.Exts (IsList(..))
 
 type Function = Double -> Double
@@ -77,6 +80,12 @@ diff :: Polynomial -> Polynomial
 diff (Polynomial cs) = Polynomial $ case cs of
   [] -> [] -- derivative of zero is zero
   (_:cs') -> zipWith (*) cs' [1..]
+
+-- | Compute the antiderivative of a polynomial.
+-- This selects C = 0.
+int :: Polynomial -> Polynomial
+int (Polynomial cs) = Polynomial (0 : cs') where
+  cs' = zipWith (/) cs [1..] where
 
 -- | A row is just a list of entries.
 type Row = [Double]
@@ -186,4 +195,136 @@ showMatrix' :: Maybe Matrix -> IO ()
 showMatrix' Nothing = pure ()
 showMatrix' (Just m) = putStr (showMatrix m)
 
+-- | Solve an upper triangular matrix by backsubstitution.
+-- This is the last step of Gaussian elimination.
 backSubstitute :: Matrix -> Row
+backSubstitute (Matrix [] []) = []
+backSubstitute (Matrix (r:rs) (c:cs)) =
+  -- solve the smaller upper triangular matrix obtained from the subsequent
+  -- rows and by dropping the zeros in the current column
+  let as = backSubstitute (Matrix (tail <$> rs) cs)
+  -- `as` is an assignment to variables x_{k+1} to x_n
+  -- note that `r` is a row with coefficients for variables x_k to x_n
+  -- so next we perform the assignment to the variables and compute
+  -- c' = r_{k+1} x_{k+1} + ... + r_n x_n
+      c' = sum (zipWith (*) (tail r) as)
+  -- finally, the assignment to x_k is (c - c') / r_k
+  -- (note r_k will be 1 if the triangular matrix was obtained from
+  -- `triangularize` above.)
+      in (c - c') / head r : as
+
+-- | Solve an augmented matrix by Gaussian elimination.
+-- First, triangularizes the matrix, and then back-substitutes.
+-- If the matrix is singular, we get back Nothing.
+gaussianSolve :: Matrix -> Maybe Row
+gaussianSolve = fmap backSubstitute . triangularize
+
+type Coordinates = [(Double, Double)]
+
+theData :: Coordinates
+theData = f <$> xs where
+  xs = [0, 0.25, 0.5, 0.75, 1.0]
+  f x = (x, sin (pi * x))
+
+-- | From data points, constructs the matrix to solve to get out the fitting
+-- parameters for the natural cubic spline interpolation.
+naturalCubicSplineMatrix
+  :: Coordinates -- ^ coordinates to interpolate
+  -> ([Double], Matrix)
+  -- ^ the h_j values and the matrix to solve
+naturalCubicSplineMatrix cs =
+  (hjs, Matrix (r : rs) (head bjs : bs)) where
+    r :: Row
+    r = (head gammajs) : (head hjs) : replicate k' 0
+    (k', Matrix rs bs) =
+      go []
+        (init $ tail $ tail hjs) -- length: n-4
+        (zip3 (tail gammajs) (tail $ tail hjs) (tail bjs))
+        --    ^ n-3          ^ n-3             ^ n-3
+
+    xjs = fst <$> cs -- length: n
+    fjs = snd <$> cs
+
+    -- the list of deltas in the x positions
+    hjs = zipWith (-) (tail xjs) xjs -- length n-1
+    -- the list of deltas in the y positions
+    dfjs = zipWith (-) (tail fjs) fjs -- length n-1
+
+    -- length: n-2
+    gammajs = zipWith g hjs (tail hjs) where
+      g hjm1 hj = 2 * (hj + hjm1)
+
+    -- length: n-2
+    bjs = zipWith g (zip dfjs hjs) (zip (tail dfjs) (tail hjs)) where
+      g (dfjm1, hjm1) (dfj, hj) = 6 * ( (dfj / hj) - (dfjm1 / hjm1) )
+
+    go
+      :: Row -- ^ the partial row of zeros that precedes hjm1 gammaj hj
+      -> [Double]
+      -- ^ h_j
+      -> [(Double, Double, Double)]
+      -- ^ gamma_j h_{j - 1} b_j
+      -> (Int, Matrix)
+      -- ^ the number of zeros to put on the end of a new row, the matrix
+    go zs [] [(gammaj, hjm1, bj)] = (0, Matrix [zs ++ hjm1 : [gammaj]] [bj])
+    go zs (hj:hjs) ((gammaj, hjm1, bj) : qs) =
+      case go (0 : zs) hjs qs of
+        (k, Matrix rs cs) ->
+          ( k + 1
+          , Matrix ((zs ++ hjm1 : gammaj : hj : replicate k 0) : rs) (bj : cs)
+          )
+    go zs hjs gs = error $
+      "bogus data " ++ show zs ++ " " ++ show hjs ++ " " ++ show gs
+
+-- construct the cubic spline matrix, solve it, and stick zero for y''_1 and
+-- y''_n
+-- Now we have all the fitting parameters
+cubicSplines :: Coordinates -> Maybe [Polynomial]
+cubicSplines (sortBy (comparing fst) -> cs) = do
+  let fjs = snd <$> cs
+  let xjs = fst <$> cs
+
+  (hjs, y''js) <-
+    fmap (fmap ((0.0:) . (++ [0.0])))
+    $ sequence $ gaussianSolve
+    <$> naturalCubicSplineMatrix cs
+
+  let mkcoeff (fj, y''j, hj) = (fj / hj) - y''j * hj / 6
+  let mkgreek y''j hj = y''j / (6 * hj)
+
+  let cjs = mkcoeff <$> zip3 (tail fjs) (tail y''js) hjs
+  let djs = mkcoeff <$> zip3 fjs y''js hjs
+  let psijs = zipWith mkgreek y''js hjs
+  let phijs = zipWith mkgreek (tail y''js) hjs
+
+  let mkpoly (phij, psij, xj1, xj, cj, dj)
+        = [ xj1**3 * psij
+            + xj**3 * phij
+            + dj * xj1
+            - cj * xj
+          , cj - dj
+            + 3 * xj**2 * phij
+            - 3 * xj1**2 * psij
+          , 3 * xj1 * psij
+            - 3 * xj * phij
+          , phij - psij
+          ]
+
+  pure $ mkpoly <$> zip6 phijs psijs (tail xjs) xjs cjs djs
+
+cubicSpline :: Coordinates -> Maybe Function
+cubicSpline cs = do
+  ps <- cubicSplines cs
+  let f ((p, xj, xj1):pxjs) x =
+        if x < xj then 0 else
+        if x < xj1 then eval p x else
+        f pxjs x
+      f [] _ = 0
+
+  pure $ f (zip3 ps (fst <$> cs) (tail $ fst <$> cs))
+
+integrateBySplines :: Coordinates -> Maybe Double
+integrateBySplines cs = do
+  ps <- fmap int <$> cubicSplines cs
+  let i (eval -> f, xj, xj1) = f xj1 - f xj
+  pure $ sum $ map i (zip3 ps (fst <$> cs) (tail $ fst <$> cs))
